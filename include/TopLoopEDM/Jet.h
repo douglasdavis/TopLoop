c03ca257e20028309a357252eb64ae353b1ff836
Douglas Davis <douglas.davis@cern.ch>
2016-03-31 22:50:21 +0200

    fix multiple definition error

--- include/TopLoopEDM/Jet.h
+++ include/TopLoopEDM/Jet.h
@@ -26,7 +26,7 @@ namespace TL {
       float m_mv2c20;
       float m_ip3dsv1;
       float m_jvt;
-      bool m_tagged;
+      bool  m_tagged;
       
       ClassDef(Jet,1);
       
@@ -48,18 +48,23 @@ namespace TL {
       float mv2c20() const;
       float ip3dsv1() const;
       float jvt() const;
-      bool isTagged( WP_mv2c20 wp = kEFF_77 ) const;
+
+      bool isTagged(WP_mv2c20 wp = WP_mv2c20::kEFF_77) const;
     };
 
-#ifndef __CINT__
-    std::map<Jet::WP_mv2c20,float> kBTAGCUTS =
-    {{Jet::kEFF_60, 0.4496}, {Jet::kEFF_70, -0.0436}, {Jet::kEFF_77, -0.4434},
-      {Jet::kEFF_85, -0.7887}};
-#endif
   }
 }
 
-
+namespace TL {
+  namespace EDM {
+    const std::map<TL::EDM::Jet::WP_mv2c20,float> kBTAGCUTS = {
+      { TL::EDM::Jet::WP_mv2c20::kEFF_60,  0.4496 },
+      { TL::EDM::Jet::WP_mv2c20::kEFF_70, -0.0436 },
+      { TL::EDM::Jet::WP_mv2c20::kEFF_77, -0.4434 },
+      { TL::EDM::Jet::WP_mv2c20::kEFF_85, -0.7887 }
+    };
+  }
+}
 
 inline void TL::EDM::Jet::set_mv2c00(const float val)   { m_mv2c00  = val; }
 inline void TL::EDM::Jet::set_mv2c10(const float val)   { m_mv2c10  = val; }
@@ -72,6 +77,9 @@ inline float TL::EDM::Jet::mv2c10()  const { return m_mv2c10;  }
 inline float TL::EDM::Jet::mv2c20()  const { return m_mv2c20;  }
 inline float TL::EDM::Jet::ip3dsv1() const { return m_ip3dsv1; }
 inline float TL::EDM::Jet::jvt()     const { return m_jvt;     }
-inline bool  TL::EDM::Jet::isTagged( WP_mv2c20 wp) const { return (m_mv2c20 > TL::EDM::kBTAGCUTS.at(wp)); }
+
+inline bool  TL::EDM::Jet::isTagged(TL::EDM::Jet::WP_mv2c20 wp) const {
+  return (m_mv2c20 > TL::EDM::kBTAGCUTS.at(wp));
+}
 
 #endif
