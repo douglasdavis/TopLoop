a9a481743945541340dcd10a4766aa122e851f08
Douglas Davis <ddavis@cern.ch>
2018-04-20 17:11:00 -0400

    Adopt ATLAS CMake recommendation, use modern CMake
    Attila recommends `atlas_add_dictionary` over
    `atlas_add_root_dictionary`.
    
    Also use the `target_compile_features` function which is part of
    "modern cmake". Using it to enforse C++14

---
+++ TopLoop/TopLoopDict.h
@@ -0,0 +1,16 @@
+#ifndef TL_TopLoopDict_h
+#define TL_TopLoopDict_h
+
+#include "TopLoop/Core/Variables.h"
+#include "TopLoop/Core/FileManager.h"
+#include "TopLoop/Core/Algorithm.h"
+#include "TopLoop/Core/Job.h"
+#include "TopLoop/Core/SampleMetaSvc.h"
+#include "TopLoop/EDM/PhysicsObject.h"
+#include "TopLoop/EDM/Lepton.h"
+#include "TopLoop/EDM/LeptonPair.h"
+#include "TopLoop/EDM/Jet.h"
+#include "TopLoop/EDM/MissingET.h"
+#include "TopLoop/EDM/FinalState.h"
+
+#endif
